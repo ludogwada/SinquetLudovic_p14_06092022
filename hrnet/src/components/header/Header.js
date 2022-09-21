6e9802218862f9537e03b7a0a2f3298312a19c5c
ludogwada <83616314+ludogwada@users.noreply.github.com>
2022-09-21 10:40:53 -0400

    styles(header): responsive

--- hrnet/src/components/header/Header.js
+++ hrnet/src/components/header/Header.js
@@ -9,7 +9,9 @@ const Header = () => {
 				<img className='header__logo' src={Logo} alt='Wealth Health' />
 				<h1 className='header__title'>HRnet</h1>
 			</section>
-			<NavBar />
+			<section className='header__navBar'>
+				<NavBar />
+			</section>
 		</header>
 	);
 };
